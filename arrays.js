cc403ef5fca306779b96e9ba8367750bdc58d2d9
Abel Enrique Cruz <abelenriquecruz@gmail.com>
2018-07-24 18:27:41 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,24 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray() {
+var superheroines = ["catwoman", "she-hulk", "mystique"]
+superheroines.unshift("wonder woman")
+console.log(superheroines)
+}
+return addElementToBeginningOfArray()
+
+function destructivelyAddElementToBeginningOfArray() {
+
+}
+
+function addElementToEndOfArray() {
+
+}
+
+function destructivelyAddElementToEndOfArray() {
+
+}
+
+function destructivelyRemoveElementFromBeginningOfArray() {
+
+}
